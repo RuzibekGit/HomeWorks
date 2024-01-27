95b96f0e7b223a4d59fc06e1d0c0859067933629
NickOneRG <robidron0199@gmail.com>
2024-01-27 23:07:58 +0500

    Yordamchi ustozga eslatmagit status!

--- M5_L3/0138_Isfandiyor.py
+++ M5_L3/0138_Isfandiyor.py
@@ -1,1 +1,6 @@
 print(((n := int(input()))**5)+(8*n**4)-(5*n**3)+(3*n**2)+n-12)
+
+#-------------------!!!-Yordamchi-ustozga-eslatma-!!!--------------------
+#
+# RoboContest  https://robocontest.uz/profile/ruzibek
+# erp.student  ga joylash vaqtida Enter qoqisdan bosilib ketdi va robocontest linkini joylolmadim
