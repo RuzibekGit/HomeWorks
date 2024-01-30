406f40ea2d676b0f42e6665a06b65a826b5a60a1
NickOneRG <robidron0199@gmail.com>
2024-01-30 22:00:52 +0500

    *

--- M5_L4/0113.Baho.py
+++ M5_L4/0113.Baho.py
@@ -1,2 +1,2 @@
-baho = int(input())
-print(new if (new := ((baho//5)*5+5))-baho < 3 and baho >= 38 else baho)
+
+print(new if (new := (((baho := int(input()))//5)*5+5))-baho < 3 and baho >= 38 else baho)
