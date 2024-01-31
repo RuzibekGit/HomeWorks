ea116c5053f49bc31b4766d00b1769a27744aaf1
NickOneRG <robidron0199@gmail.com>
2024-01-31 20:39:17 +0500

    Oqishga qulay versiya qoshildi

--- M5_L4/0082.Toshlar-oyini.py
+++ M5_L4/0082.Toshlar-oyini.py
@@ -1,1 +1,9 @@
 print("Second player" if int(input()) % 2 == 0 else "First player")
+
+
+# N = int(input())  #  Oqishga qulay versiya
+
+# if N % 2 == 0 :
+#     print("Second player")
+# else:
+#     print("First player")
--- M5_L4/0116.Azimjonning-qoylari.py
+++ M5_L4/0116.Azimjonning-qoylari.py
@@ -1,1 +1,12 @@
 print(jami//2 if (jami:= int(input()))%4 == 0 else -1)
+
+
+
+# jami = int(input()) # Oqishga qulay versiya
+
+# if jami % 4 == 0: 
+#     print(jami//2)
+# else:
+#     print(jami)
+
+
